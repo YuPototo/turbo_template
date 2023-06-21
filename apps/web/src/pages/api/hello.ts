7020f8c7e8eae9e4bd0c9ce55b68295194711480
Yu Qin <sirius.qinyu@gmail.com>
2023-06-22 07:10:00 +0900

    Rename database package to prisma
    More descriptive name for the package

--- apps/web/src/pages/api/hello.ts
+++ apps/web/src/pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { PrismaClient } from "@company-name/database";
+import { PrismaClient } from "@company-name/prisma";
 
 type Data = {
   posts: any[];
