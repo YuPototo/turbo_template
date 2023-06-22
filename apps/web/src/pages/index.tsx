b5b7584a33cdd96a64ab71abeab473fc5a1307a6
Yu Qin <sirius.qinyu@gmail.com>
2023-06-22 11:11:36 +0900

    integrate TRPC

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,19 +1,16 @@
+import { api } from "@/utils/api";
+
 import { Button } from "@company-name/ui";
 
 export default function Home() {
-  const fetchPost = async () => {
-    const response = await fetch("/api/hello");
-    const data = await response.json();
-
-    console.log(data);
-  };
-
-  fetchPost();
+  const dumbQuery = api.post.dumbEndpoint.useQuery();
 
   return (
     <>
       <Button />
       <div className="bg-blue-500 p-2">abc</div>
+
+      {dumbQuery.data ? <div>{dumbQuery.data}</div> : null}
     </>
   );
 }
