f1a39a773655e6e5fdbb5d593219fa85b88e0039
Yu Qin <sirius.qinyu@gmail.com>
2023-06-24 10:37:14 +0900

    auth done

---
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,5 @@
+import NextAuth from "next-auth";
+
+import { authOptions } from "@company-name/auth";
+
+export default NextAuth(authOptions);
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { RouterOutputs, api } from "@/utils/api";
+import { signIn, signOut } from "next-auth/react";
 import { useState } from "react";
 
 import { Button } from "@company-name/ui";
@@ -66,6 +67,31 @@ const CreatePostForm: React.FC = () => {
   );
 };
 
+const AuthShowcase: React.FC = () => {
+  const { data: session } = api.auth.getSession.useQuery();
+
+  const { data: secretMessage } = api.auth.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: !!session?.user }
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      {session?.user && (
+        <p className="text-center text-2xl ">
+          {session && <span>Logged in as {session?.user?.name}</span>}
+          {secretMessage && <span> - {secretMessage}</span>}
+        </p>
+      )}
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        onClick={session ? () => void signOut() : () => void signIn()}>
+        {session ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
+
 export default function Home() {
   const postQuery = api.post.all.useQuery();
 
@@ -76,6 +102,7 @@ export default function Home() {
   return (
     <>
       <Button />
+      <AuthShowcase />
       <CreatePostForm />
 
       {postQuery.data ? (
